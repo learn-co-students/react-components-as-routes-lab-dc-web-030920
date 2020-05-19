dec4cb48c1620c97d1a2067a4fe57defd954cc35
lmonty22 <lindsay.montgomery@olympian.org>
2020-05-18 20:20:59 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,11 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+  {actors.map(a => {
+    return<div>{a.name}
+     <ul>{a.movies.map(m=> {return <li>{m}</li>})}
+     </ul></div>})}
     </div>
   );
 };
